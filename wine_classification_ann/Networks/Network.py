8f35e0d57162fc0abb1e4b1746bcccb7f8005ef7
Danielr1312 <rodriguez.da@ufl.edu>
2024-02-29 00:29:54 -0500

    moved functions and classes into folders, created train and test functions in Utilities.run_experiments, modified first experiment to make use of new functions, organized code. TODO: finish updating other experiments, maybe add some plots, make readme, modify results storage to save results in a dictionary with a unique key for each combination

--- wine_classification_ann/Networks/Network.py
+++ wine_classification_ann/Networks/Network.py
@@ -1,38 +1,164 @@
 import torch
 from torch import nn
 
+
 class Network(nn.Module):
-    def __init__(self, num_hidden): # here we can play with the number of hidden layers 
+    """
+    A flexible neural network class that can be used to create a variety of network architectures 
+    ranging from simple linear models to more complex multi-layer perceptrons.
+    
+    Parameters:
+        n_inputs (int): The number of input features for the network.
+        n_outputs (int): The number of output classes for the network.
+        num_hidden_layers (int, optional): The number of hidden layers in the network. 
+                                            Defaults to 0, which results in a linear model.
+        hidden_sizes (list of int, optional): A list containing the sizes of each hidden layer. 
+                                              The length of this list should match num_hidden_layers. 
+                                              Defaults to an empty list, and if num_hidden_layers is 
+                                              greater than 0 but hidden_sizes is empty, each hidden 
+                                              layer defaults to a size of 10 neurons.
+    
+    Methods:
+        forward(x): Performs a forward pass through the network with input x.
+                    Parameters:
+                        x (Tensor): A tensor containing input data.
+                    Returns:
+                        Tensor: The network's output tensor.
+                        
+        prediction(output): Converts the network's output tensor into predicted class labels.
+                            Parameters:
+                                output (Tensor): A tensor containing the network's raw output.
+                            Returns:
+                                Tensor: A tensor containing predicted class labels starting from 1.
+    """
+    def __init__(self, n_inputs, n_outputs, num_hidden_layers=0, hidden_sizes=[]):
         super(Network, self).__init__()
-        self.num_hidden = num_hidden
+        self.num_hidden_layers = num_hidden_layers
         
-        # Inputs to hidden linear combination
-        self.hidden = nn.Linear(13, self.num_hidden) # We have 13 predictors
-        # hidden to output layer, 3 classes - one for each cultivar
-        self.output = nn.Linear(self.num_hidden, 3) # We have 3 target classes to predict
+        layers = []
+        last_size = n_inputs  # Set the size of the last layer to the input size initially
         
-        # Defining activation functions
-        self.sigmoid = nn.Sigmoid() # for PyTorch it knows that calling nn.Sigmoid requires its derivative as well
-        # it creates something called dynamic computational graph which is a graph of the gradient (it computes every epoch)
+        # Create hidden layers if num_hidden_layers > 0
+        if num_hidden_layers > 0:
+            for i in range(num_hidden_layers):
+                # Use the provided hidden size for each layer, default to 10 if not enough sizes provided
+                hidden_size = hidden_sizes[i] if i < len(hidden_sizes) else 10
+                layers.append(nn.Linear(last_size, hidden_size))
+                layers.append(nn.Sigmoid())
+                last_size = hidden_size  # Update last_size to the current hidden size
+        
+        # Output layer
+        layers.append(nn.Linear(last_size, n_outputs))
+        
+        # Combine all layers
+        self.layers = nn.Sequential(*layers)
         
     def forward(self, x):
-        z1 = self.hidden(x)
-        out1 = self.sigmoid(z1)
-        z2 = self.output(out1)
-        #out2 = self.sigmoid(z2)
-        
-        return z2
+        return self.layers(x)
     
     def prediction(self, output):
-        preds = torch.zeros(1,output.shape[0]).flatten().long()
-        for i in range(len(preds)):
-            index = torch.argmax(output[i,:])
-            
-            if index == 0:
-                preds[i] = 1
-            elif index == 1:
-                preds[i] = 2
-            else:
-                preds[i] = 3
-                
+        preds = torch.argmax(output, dim=1) + 1  
         return preds
+
+# class Network(nn.Module):
+#     def __init__(self, num_hidden): # here we can play with the number of hidden layers 
+#         super(Network, self).__init__()
+#         self.num_hidden = num_hidden
+        
+#         # Inputs to hidden linear combination
+#         self.hidden = nn.Linear(13, self.num_hidden) # We have 13 predictors
+#         # hidden to output layer, 3 classes - one for each cultivar
+#         self.output = nn.Linear(self.num_hidden, 3) # We have 3 target classes to predict
+        
+#         # Defining activation functions
+#         self.sigmoid = nn.Sigmoid() # for PyTorch it knows that calling nn.Sigmoid requires its derivative as well
+#         # it creates something called dynamic computational graph which is a graph of the gradient (it computes every epoch)
+        
+#     def forward(self, x):
+#         z1 = self.hidden(x)
+#         out1 = self.sigmoid(z1)
+#         z2 = self.output(out1)
+#         #out2 = self.sigmoid(z2)
+        
+#         return z2
+    
+#     def prediction(self, output):
+#         preds = torch.zeros(1,output.shape[0]).flatten().long()
+#         for i in range(len(preds)):
+#             index = torch.argmax(output[i,:])
+            
+#             if index == 0:
+#                 preds[i] = 1
+#             elif index == 1:
+#                 preds[i] = 2
+#             else:
+#                 preds[i] = 3
+                
+#         return preds
+    
+# class Network2(nn.Module):
+#     def __init__(self, num_hidden1, num_hidden2):
+#         super(Network2, self).__init__()
+#         self.num_hidden1 = num_hidden1
+#         self.num_hidden2 = num_hidden2
+        
+#         # Inputs to hidden linear combination
+#         self.hidden1 = nn.Linear(13, self.num_hidden1)
+#         # hidden to output layer, 3 classes - one for each cultivar
+#         self.hidden2 = nn.Linear(self.num_hidden1, self.num_hidden2)
+#         self.out = nn.Linear(self.num_hidden2, 3)
+        
+#         # Defining activation functions
+#         self.sigmoid = nn.Sigmoid()
+        
+#     def forward(self, x):
+#         z1 = self.hidden1(x)
+#         out1 = self.sigmoid(z1)
+#         z2 = self.hidden2(out1)
+#         out2 = self.sigmoid(z2)
+#         z3 = self.out(out2)
+        
+#         return z3
+    
+#     def prediction(self, output):
+#         preds = torch.zeros(1,output.shape[0]).flatten().long()
+#         for i in range(len(preds)):
+#             index = torch.argmax(output[i,:])
+            
+#             if index == 0:
+#                 preds[i] = 1
+#             elif index == 1:
+#                 preds[i] = 2
+#             else:
+#                 preds[i] = 3
+                
+#         return preds
+    
+# class Network3(nn.Module):
+#     def __init__(self):
+#         super(Network3, self).__init__()
+        
+#         # Inputs to outputs linear combination
+#         self.out = nn.Linear(13, 3)
+        
+#         # Defining activation functions
+#         self.sigmoid = nn.Sigmoid()
+        
+#     def forward(self, x):
+#         z1 = self.out(x)
+        
+#         return z1
+    
+#     def prediction(self, output):
+#         preds = torch.zeros(1,output.shape[0]).flatten().long()
+#         for i in range(len(preds)):
+#             index = torch.argmax(output[i,:])
+            
+#             if index == 0:
+#                 preds[i] = 1
+#             elif index == 1:
+#                 preds[i] = 2
+#             else:
+#                 preds[i] = 3
+                
+#         return preds
--- wine_classification_ann/Utilities/utils.py
+++ wine_classification_ann/Utilities/utils.py
@@ -48,3 +48,47 @@ def ConfusionMatrix(model, output, target):
             CM[combined[i, 0] - 1, combined[i, 1] - 1] += 1
                     
     return CM
+
+def convert_results_to_dict(params, results):
+    """
+    Converts a list of lists containing results into a dictionary with labels corresponding to the values in params.
+    
+    Parameters:
+        params (dict): A dictionary containing parameter values, with keys 'hidden_size' and 'learning_rate'.
+        results (list of lists): A list where each sublist contains results corresponding to a combination of 
+                                 'hidden_size' and 'learning_rate' from the params.
+    
+    Returns:
+        dict: A nested dictionary where each 'hidden_size' maps to a sub-dictionary where each 'learning_rate'
+              maps to the corresponding result.
+    """
+    results_dict = {}
+    for i, hidden_size in enumerate(params['hidden_size']):
+        # Prepare a sub-dictionary for each hidden_size
+        hidden_size_results = {}
+        for j, learning_rate in enumerate(params['learning_rate']):
+            # Check if the current index is within the bounds of the result list to avoid IndexError
+            if i < len(results) and j < len(results[i]):
+                # Assign each result to its corresponding learning rate
+                hidden_size_results[learning_rate] = results[i][j]
+        # Convert hidden_size to string to use as a dictionary key
+        results_dict[str(hidden_size)] = hidden_size_results
+    return results_dict
+
+def convert_all_results(params, results_dict):
+    """
+    Converts multiple lists of results into dictionaries with labels corresponding to the values in params.
+    
+    Parameters:
+        params (dict): A dictionary containing parameter values, with keys like 'hidden_size' and 'learning_rate'.
+        results_dict (dict): A dictionary where each key is a string representing the type of result (e.g., 
+                             'training_losses') and each value is a list of lists containing the results.
+    
+    Returns:
+        dict: A nested dictionary where each type of result maps to another dictionary structured by 'hidden_size'
+              and 'learning_rate' according to the params.
+    """
+    converted_results = {}
+    for key, result_lists in results_dict.items():
+        converted_results[key] = convert_results_to_dict(params, result_lists)
+    return converted_results
